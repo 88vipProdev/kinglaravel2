d1229290991aa81822db588e3056bacb8c3ae5b3
88vipProdev <nguyentunglam18091998abc@gmail.com>
2024-07-28 17:56:04 +0700

    create categories

--- app/Http/Controllers/categoriesController.php
+++ app/Http/Controllers/categoriesController.php
@@ -2,22 +2,19 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Http\Requests\categories;
+use App\Http\Requests\CategoryRequest;
+use App\Models\categories;
 
 class categoriesController extends Controller
 {
-    
-
         public function createCategories()
         {
-            return View('categories');
+            return view('categories');
         }
 
-        // public function add(categories $request)
-        // {   $categories = new categories();
-        //      $categories->name =$request->name;
-        //     $categories->save();
-        // }
+        public function add(CategoryRequest $request )
+        {
+            categories::create($request->all());
+        }
 
 }
